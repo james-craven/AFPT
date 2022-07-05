06c7ba088df456484f356ac12d75bc7bd974624d
James Craven <jamescraven@Jamess-MacBook-Air.local>
2022-07-04 21:19:19 -0500

    move service worker registration sooner

--- sw-registration.js
+++ sw-registration.js
@@ -1,11 +1,11 @@
 (async () => {
     if ("serviceWorker" in navigator) {
         let t = !1;
-        const e = await navigator.serviceWorker.register("sw.js", {scope: '/'});
+        const e = await navigator.serviceWorker.register("sw.js", {scope: 'https://afptcalc.com'});
         e.addEventListener("updatefound", () => {
             e.installing &&
                 e.installing.addEventListener("statechange", () => {
-                    e.waiting && (navigator.serviceWorker.controller ? t || ((t = !0), window.location.reload()) : console.log("Service Worker initialized for the first time"));
+                    e.waiting && (navigator.serviceWorker.controller ? t || ((t = !0), window.location.reload()) : console.log("Service Worker initialized for the first time: " + e.scope));
                 });
         });
     }
