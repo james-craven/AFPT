9408a67bd645bd7bcf85c262ce3ba75a07292ffc
James <jrcdawg1994@gmail.com>
2022-11-12 08:50:12 +0100

    fixed caching index.html

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	globDirectory: './',
 	globPatterns: [
-		'**/*.{html,png,webmanifest,js,m4a,mp3,ogg,css,mjs,webp}'
+		'**/*.{png,webmanifest,m4a,mp3,ogg,mjs,webp}'
 	],
 	swDest: 'sw.js',
 	ignoreURLParametersMatching: [
